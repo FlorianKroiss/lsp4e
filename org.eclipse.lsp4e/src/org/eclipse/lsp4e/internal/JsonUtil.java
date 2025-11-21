8de45817d8d55615eb2aa850075dc6e8efc7097f
Kroiß, Florian <florian.kroiss@vector.com>
2025-11-21 13:58:30 +0100

    fix: Use a Gson singleton which knows about the types from LSP4J

---
+++ org.eclipse.lsp4e/src/org/eclipse/lsp4e/internal/JsonUtil.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   See git history
+ *******************************************************************************/
+package org.eclipse.lsp4e.internal;
+
+import java.util.Map;
+import java.util.Objects;
+
+import org.eclipse.lsp4j.jsonrpc.json.MessageJsonHandler;
+
+import com.google.gson.Gson;
+
+/**
+ * Provides a {@link Gson} instance which can properly serialize and deserialize LSP4J JSON-RPC objects
+ */
+public class JsonUtil {
+
+	private static final Gson LSP4J_GSON = Objects.requireNonNull(new MessageJsonHandler(Map.of()).getGson());
+
+	/**
+	 * Obtain a singleton Gson instance to work with LSP4J JSON objects.
+	 */
+	public static Gson getLSP4J_GSON() {
+		return LSP4J_GSON;
+	}
+
+}
